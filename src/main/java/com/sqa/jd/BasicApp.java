7c9a07a0e929edc618119bc3e80e43c2577330fd
Javier Delgado <texano8@hotmail.com>
2017-08-12 14:15:46 -0700

    Create project Structure

---
+++ src/main/java/com/sqa/jd/BasicApp.java
@@ -0,0 +1,34 @@
+/**
+ *   File Name: BasicApp.java<br>
+ *
+ *   Nepton, Jean-francois<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Aug 12, 2017
+ *   
+ */
+
+package com.sqa.jd;
+
+
+/**
+ * BasicApp //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Nepton, Jean-francois
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class BasicApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
