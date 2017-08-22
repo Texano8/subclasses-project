20d0c39c8456fef77fafd9ba6060628adf0384bf
Javier Delgado <texano8@hotmail.com>
2017-08-21 19:09:01 -0700

    Create AppBasics Helper Class

--- src/main/java/com/sqa/jd/BasicApp.java
+++ src/main/java/com/sqa/jd/BasicApp.java
@@ -1,15 +1,11 @@
 /**
- *   File Name: BasicApp.java<br>
- *
- *   Nepton, Jean-francois<br>
- *   Java Boot Camp Exercise<br>
- *   Instructor: Jean-francois Nepton<br>
- *   Created: Aug 12, 2017
- *   
+ * File Name: BasicApp.java<br>
+ * Nepton, Jean-francois<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Aug 12, 2017
  */
-
 package com.sqa.jd;
-
 
 /**
  * BasicApp //ADDD (description of class)
@@ -17,17 +13,14 @@ package com.sqa.jd;
  * //ADDD (description of core fields)
  * <p>
  * //ADDD (description of core methods)
- * 
- * @author      Nepton, Jean-francois
- * @version     1.0.0
- * @since       1.0
  *
+ * @author Javier Delgado
+ * @version 1.0.0
+ * @since 1.0
  */
 public class BasicApp {
+	// this project is to include AppBasics with new requestFloat method
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	}
---
+++ src/main/java/com/sqa/jd/helpers/AppBasics.java
@@ -0,0 +1,27 @@
+/**
+ * File Name: AppBasics.java<br>
+ * Javier Delgado<br>
+ * Date Aug 19/2017
+ */
+package com.sqa.jd.helpers;
+
+/**
+ * AppBasics //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Javier Delgado
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class AppBasics {
+
+	/**
+	 * @param args
+	 */
+	public static double requestDouble(String[] question) {
+		return 0;
+	}
+}
