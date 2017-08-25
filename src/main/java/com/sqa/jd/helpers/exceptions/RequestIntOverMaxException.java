748184a74af5470ada641876a0300d78d5e8bfa5
Javier Delgado <texano8@hotmail.com>
2017-08-24 18:50:25 -0700

    Create two exceptions

---
+++ src/main/java/com/sqa/jd/helpers/exceptions/RequestIntOverMaxException.java
@@ -0,0 +1,24 @@
+/**
+ *   File Name: RequestIntOverMaxException.java<br>
+ *
+ *   Javier Delgado<br>
+ *   Date Aug 19/2017
+ */
+
+package com.sqa.jd.helpers.exceptions;
+
+
+/**
+ * RequestIntOverMaxException //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Javier Delgado
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class RequestIntOverMaxException extends Exception {
+}
---
+++ src/main/java/com/sqa/jd/helpers/exceptions/RequestIntUnderMinException.java
@@ -0,0 +1,24 @@
+/**
+ *   File Name: RequestIntUnderMinException.java<br>
+ *
+ *   Javier Delgado<br>
+ *   Date Aug 19/2017
+ */
+
+package com.sqa.jd.helpers.exceptions;
+
+
+/**
+ * RequestIntUnderMinException //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Javier Delgado
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class RequestIntUnderMinException extends Exception {
+}
